bda7e3568868697e2d3256d28fe0aa69cef2edf6
ERROR <fsadakathussin@gmail.com>
2019-02-27 21:55:17 +0600

    initial commit

--- app/src/main/java/go/faddy/com/agin2/CalenderFrag.java
+++ app/src/main/java/go/faddy/com/agin2/CalenderFrag.java
@@ -10,25 +10,32 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.Toast;
 
 import com.applandeo.materialcalendarview.CalendarView;
 import com.applandeo.materialcalendarview.EventDay;
 import com.applandeo.materialcalendarview.listeners.OnDayClickListener;
 import com.applandeo.materialcalendarview.listeners.OnSelectDateListener;
 
+import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
+
+import go.faddy.com.agin2.utils.DrawableUtils;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class CalenderFrag extends Fragment implements OnSelectDateListener {
-
+    public static String STRING_EXTRA = "CAL";
     RadioGroup radioGroup;
     RadioButton radioButton, getRadio, postRadio;
-
-
+    public int day, month, year;
+    public String day_, month_, year_;
+    CalendarView calendarView;
+    Intent intent;
     public CalenderFrag() {
         // Required empty public constructor
     }
@@ -43,23 +50,51 @@ public class CalenderFrag extends Fragment implements OnSelectDateListener {
         radioGroup = v.findViewById(R.id.radiogroup);
 
 
-
+        List<EventDay> events = new ArrayList<>();
+//        Calendar calendar = Calendar.getInstance();
+//
+//        events.add(new EventDay(calendar, DrawableUtils.getCircleDrawableWithText(getContext(), "CT")));
+        Calendar calendar1 = Calendar.getInstance();
+        calendar1.add(Calendar.DAY_OF_MONTH, 0);
+        events.add(new EventDay(calendar1, DrawableUtils.getCircleDrawableWithText(getContext(), "ASS")));
+        calendarView.setEvents(events);
         calendarView.setOnDayClickListener(new OnDayClickListener() {
             @Override
             public void onDayClick(EventDay eventDay) {
 //                Snackbar.make(getView(), eventDay.getCalendar().getTime().toString() +
 //                        eventDay.isEnabled(), Snackbar.LENGTH_LONG)
 //                        .setAction("Action", null).show();
+                intent = new Intent(getActivity().getApplicationContext(), Pop.class);
+
+                Date y = eventDay.getCalendar().getTime();
+//                Toast.makeText(getContext(), r , Toast.LENGTH_SHORT).show();
+                intent.putExtra(STRING_EXTRA, y.getTime());
+
                 int radioId = radioGroup.getCheckedRadioButtonId();
                 radioButton = v.findViewById(radioId);
                 getRadio = v.findViewById(R.id.get);
                 postRadio = v.findViewById(R.id.post);
                 if(radioButton == getRadio){
-                    Snackbar.make(getView(), "Get Selected", Snackbar.LENGTH_LONG)
-                            .setAction("Action", null).show();
+                    day = eventDay.getCalendar().getTime().getDate();
+                    day_ = String.valueOf(day);
+                    month = eventDay.getCalendar().getTime().getMonth();
+                    String month_ = String.valueOf(month + 1);
+                    year = eventDay.getCalendar().getTime().getYear();
+                    year_ = String.valueOf(year - 100);
+                    Toast.makeText(getContext(), day + "\n" + month_ + "\n" + year_, Toast.LENGTH_SHORT).show();
+
+//                    Snackbar.make(getView(), (int) d, Snackbar.LENGTH_LONG)
+//                           .setAction("Action", null).show();
                 }
                 else if(radioButton == postRadio){
-                    startActivity(new Intent(getContext(), Pop.class));
+                    day = eventDay.getCalendar().getTime().getDate();
+                    month = eventDay.getCalendar().getTime().getMonth();
+                    year = eventDay.getCalendar().getTime().getYear();
+                    day_ = String.valueOf(day);
+                    month_ = String.valueOf(month + 1);
+                    year_ = String.valueOf(year - 100);
+
+                    startActivity(intent);
 
                     Snackbar.make(getView(), "Post Selected", Snackbar.LENGTH_LONG)
                             .setAction("Action", null).show();
@@ -68,7 +103,6 @@ public class CalenderFrag extends Fragment implements OnSelectDateListener {
                     Snackbar.make(getView(), "Some error happend", Snackbar.LENGTH_LONG)
                             .setAction("Action", null).show();
                 }
-
             }
         });
         return v;
--- app/src/main/java/go/faddy/com/agin2/Pop.java
+++ app/src/main/java/go/faddy/com/agin2/Pop.java
@@ -1,11 +1,16 @@
 package go.faddy.com.agin2;
 
 import android.app.Activity;
+import android.content.ClipData;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.text.format.DateFormat;
 import android.util.DisplayMetrics;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
@@ -14,30 +19,67 @@ import android.widget.Toast;
 
 import com.weiwangcn.betterspinner.library.material.MaterialBetterSpinner;
 
+import java.util.Date;
+import java.util.Objects;
+
+import static go.faddy.com.agin2.CalenderFrag.STRING_EXTRA;
+
 public class Pop extends Activity {
     RadioButton filesYes, filesNo, radioButton;
     RadioGroup radioGroup;
     ImageButton button;
-
-
+    private static final int RQS_OPEN = 1;
+    Button buttonOpen;
+    TextView textUri;
     TextView txt;
     String[] typeList = {"Class-Test", "Home-Work", "Assignment", "Lab-Report"};
     String[] subjectList = {"Database Management", "Database Management (Sessional)", "Compiler",
-    "Compiler (Sessional)", "Microprocessors & Micro-controllers", "Microprocessors & Micro-controllers(Sessional)",
-            "Theory of Computation", "Assembely Language Programming(Sessional)",
-            "Computer Network", "Computer Network(Sessional)"
+            "Compiler (Sessional)", "Microprocessors & Micro-controllers", "Microprocessors & " +
+            "Micro-controllers(Sessional)", "Theory of Computation", "Assembely Language" +
+            " Programming(Sessional)", "Computer Network", "Computer Network(Sessional)"
     };
+    View.OnClickListener button_ =
+            new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent();
+                    intent.setAction(Intent.ACTION_OPEN_DOCUMENT);
+                    intent.addCategory(Intent.CATEGORY_OPENABLE);
+                    intent.setType("*/*");
+                    String[] extraMimeTypes = {"image/*", "video/*", "audio/*", "text/*",
+                            "application/*", "./*", "camera/*"};
+                    intent.putExtra(Intent.EXTRA_MIME_TYPES, extraMimeTypes);
+                    intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true);
+                    startActivityForResult(intent, RQS_OPEN);
+                    Toast.makeText(Pop.this,
+                            "Single-selection: Tap on any file.\n" +
+                                    "Multi-selection: Tap & Hold on the first file, " +
+                                    "tap for more, tap on OPEN to finish.",
+                            Toast.LENGTH_LONG).show();
+                }
+            };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.pop);
-
+        textUri = (TextView) findViewById(R.id.filenames);
         DisplayMetrics dm = new DisplayMetrics();
         getWindowManager().getDefaultDisplay().getMetrics(dm);
-
         int width = dm.widthPixels;
         int height = dm.heightPixels;
         getWindow().setLayout((int)(width*.8), (int)(height*.7));
+
+        //getting date intents
+        Bundle bundle = getIntent().getExtras();
+        Long good = bundle.getLong(STRING_EXTRA);
+        Date today = new Date(good);
+        String dayOfTheWeek = (String) DateFormat.format("EEEE", today);
+        String day = (String) DateFormat.format("dd", today);
+        String monthString = (String) DateFormat.format("MMM", today);
+        String monthNumber = (String) DateFormat.format("MM", today);
+        String year = (String) DateFormat.format("yyyy", today);
+        Toast.makeText(this, day + "\n" + monthNumber + "\n" + year, Toast.LENGTH_SHORT).show();
 
 
         //type drop down
@@ -59,10 +101,10 @@ public class Pop extends Activity {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 Toast.makeText(Pop.this, parent.getItemAtPosition(position).toString(), Toast.LENGTH_SHORT).show();
-
             }
         });
 
+        //Files spinner
 
         radioGroup = findViewById(R.id.files_grp);
         filesYes = findViewById(R.id.files_yes);
@@ -81,6 +123,30 @@ public class Pop extends Activity {
                 }
             }
         });
+        button.setOnClickListener(button_);
 
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        StringBuilder s = new StringBuilder();
+        if (resultCode == RESULT_OK) {
+            if (requestCode == RQS_OPEN) {
+                ClipData clipData = data.getClipData();
+                if (clipData == null) {
+                    s = new StringBuilder("clipData == null\n");
+                    s.append(Objects.requireNonNull(data.getData()).toString());
+                } else {
+                    s = new StringBuilder("clipData != null\n");
+                    for (int i = 0; i < clipData.getItemCount(); i++) {
+                        ClipData.Item item = clipData.getItemAt(i);
+                        Uri uri = item.getUri();
+                        s.append(uri.toString()).append("\n");
+                    }
+                }
+            }
+        }
+        textUri.setText(s.toString());
     }
 }
